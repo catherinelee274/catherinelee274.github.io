5e09cfcee38595ef4424bd32c2009854499ca3f2
catherinelee274 <catherinelee274@yahoo.com>
2017-06-28 23:36:30 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+
+/* TEMP */
+particlesJS.load('particles-js', 'assets/particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
