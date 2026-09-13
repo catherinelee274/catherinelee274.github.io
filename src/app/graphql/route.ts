af5eb55c4b0aced183784c304a501e4c50808c08
Catherine Lee <catherinelee274@yahoo.com>
2026-09-13 05:05:00 -0400

    Fix second build issue

--- src/app/graphql/route.ts
+++ src/app/graphql/route.ts
@@ -7,6 +7,8 @@ import { MeResolver } from "../../apollo/resolvers";
 import { buildSchema } from "type-graphql";
 import { NextRequest } from "next/server";
 
+export const dynamic = "force-static";
+
 const schema = await buildSchema({
   resolvers: [MeResolver],
 });
